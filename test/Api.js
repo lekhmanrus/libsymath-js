236f41db20cbf7215cc69f5cc644699c59e5d6e8
Denys Zariaiev <denys.zariaiev@gmail.com>
2014-02-21 15:00:04 +0200

    tree api test

--- test/Api.js
+++ test/Api.js
@@ -2,11 +2,33 @@
 /*global module, require */
 'use strict';
 
-var symath = require('../index.js');
+var symath = require('../index.js'),
+    Node   = require('../src/tree').Node,
+    Leaf   = require('../src/tree').Leaf;
 
 module.exports.integrationTest = function(test) {
   test.ok(symath.Lexer);
   test.ok(symath.Expression);
+  
+  test.done();
+};
+
+module.exports.internalTreeApiTest = function(test) {
+  var nodeProps = Object.getOwnPropertyNames(new Node()),
+      leafProps = Object.getOwnPropertyNames(new Leaf()),
+      nodeProto = Object.getOwnPropertyNames(Node.prototype),
+      leafProto = Object.getOwnPropertyNames(Leaf.prototype),
+      i;
+  
+  test.strictEqual(nodeProps.length, leafProps.length);
+  for(i = 0; i < nodeProps.length; ++i) {
+    test.notStrictEqual(leafProps.indexOf(nodeProps[i]), -1);
+  }
+  
+  test.strictEqual(nodeProto.length, leafProto.length);
+  for(i = 0; i < nodeProto.length; ++i) {
+    test.notStrictEqual(leafProto.indexOf(nodeProto[i]), -1);
+  }
   
   test.done();
 };
